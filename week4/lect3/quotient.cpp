9cdae44185e34896e37af2e3b13d10884d534d32
832004ATHARV <atharv832004@gmail.com>
2024-06-02 21:57:24 +0530

    Quotient

--- week4/lect3/quotient.cpp
+++ week4/lect3/quotient.cpp
@@ -24,20 +24,53 @@ int quotient(int divident, int divisor){
         else{
             e = mid - 1;
         }
-        mid = e + (s - e) / 2;
+       mid = e + ((s - e) >> 1);
+    }
+    return ansIndex;
+}
+
+double Precisionquotient(int divident, int divisor) {   //mega class 
+    double s = -divident;
+    double e = divident;
+    double ansIndex = -1; 
+
+    while ((e - s) > 0.001) {
+        double mid = s + (e - s) / 2.0;
+
+        double mul = divisor * mid;
+
+        if (mul < divident) {
+            s = mid;
+            ansIndex = mid;  
+        } else if (mul == divident) {
+            return mid;
+        } else {
+            e = mid;
+        }
+        mid = s + (e - s) / 2.0;
     }
     return ansIndex;
 }
 
 int main(){
 
-    int divident = -50;
-    int divisor = 5;
+    int divident = 7;
+    int divisor = -3;
     int ans = quotient(abs(divident), abs(divisor));   // idhar dono be se ek bhi negative ane par asnwer shi nhi chal rha 
 
     if ((divident < 0 && divisor > 0) || (divident > 0 && divisor < 0)) {  // isliye yeh lagaya 
         ans = 0 - ans;
     }
+     cout << "Quotient is wihtout precision is : " << ans<<endl;
 
-    cout << "Quotient is " << ans;
+
+
+     double  ans1 = Precisionquotient(abs(divident), abs(divisor));   // idhar dono be se ek bhi negative ane par asnwer shi nhi chal rha 
+
+    if ((divident < 0 && divisor > 0) || (divident > 0 && divisor < 0)) {  // isliye yeh lagaya 
+        ans1 = 0 - ans1;
+    }
+     cout << "Quotient is with precision is : " << ans1;
+
+
 }
